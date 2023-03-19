3511e6b8f23b32ef5307d05f06f504552605165f
Christian <71664957+Christian-Mar@users.noreply.github.com>
2023-03-19 19:30:28 +0100

    preparation steps + component

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 #Implementing Lexical in next.js with firebase
+
+1. Install firebase
+2. Make component fot the editor
+3. Import lexical
+4. Setup lexical 
+5. Send data to firebase
+6. Retrieve data from firbase
+7. Insert the delete function
+8. Insert the update function 
+9. Styling 
+
--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Editor from './components/editor';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Start from here
+      Setup of Lexical
+      <div>
+      <Editor/>
+      </div>
+      <div>Show the data</div>
     </main>
   )
 }
