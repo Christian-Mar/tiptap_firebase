06243d9c52a5727fe605ca5d048fe0fbf613a676
Christian <71664957+Christian-Mar@users.noreply.github.com>
2023-03-19 19:18:38 +0100

    firebase ready

---
+++ firebase/config.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+// init firebase
+
+const app = initializeApp(firebaseConfig);
+
+// init firestore
+
+const db = getFirestore(app);
+
+//  init authentication
+
+const auth = getAuth();
+
+//  init storage
+
+const storage = getStorage();
+
+export { db, auth, storage };
