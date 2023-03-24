615c8a91f7fe03a517e70a0635e7546e1ba8e6f4
Christian <71664957+Christian-Mar@users.noreply.github.com>
2023-03-24 05:19:51 +0100

    changed plain text into rich text

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -4,7 +4,7 @@ import {$getRoot, $getSelection} from 'lexical';
 import {useEffect} from 'react';
 
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 //import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
@@ -32,7 +32,7 @@ export default function Editor() {
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
+      <RichTextPlugin
         contentEditable={<ContentEditable className={styles.contentEditable}/>}
         placeholder={<div className={styles.placeholder}>Enter some text...</div>}
         ErrorBoundary={LexicalErrorBoundary}
