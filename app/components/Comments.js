06d11230dfd04613b3b2f9dcaf9be128d79c03d4
Christian <71664957+Christian-Mar@users.noreply.github.com>
2023-03-24 05:04:06 +0100

    editor started

--- app/components/Comments.js
+++ app/components/Comments.js
@@ -19,7 +19,7 @@ export default function Comments() {
 					</p>
 				</li>
 				<li>
-					<p>Can I just import the component with dynamic import and use 'ssr: false' there, istead of using it in the component itself?</p>
+					<p>Can I just import the component with dynamic import and use 'ssr: false' there, instead of using it in the component itself? Or, can I just use "use client"?</p>
 				</li>
 			</ul>
 		</div>
--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,5 +1,46 @@
+"use client"
+
+import {$getRoot, $getSelection} from 'lexical';
+import {useEffect} from 'react';
+
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+//import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+//import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import styles from '../../styles/Editor.module.css';
+
+const theme = {
+	// Theme styling goes here
+	
+	}
+
+
+
+function onError(error) {
+  console.error(error);
+}
+
 export default function Editor() {
-  return(
-    <div>Editor</div>
-  )
+  const initialConfig = {
+    namespace: 'MyEditor', 
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable className={styles.contentEditable}/>}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+        
+      />
+      
+      <HistoryPlugin />
+     
+    </LexicalComposer>
+  );
 }
