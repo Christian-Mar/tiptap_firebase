97ae561b7ff33f94b5be602c439ed9dbe20656b1
Christian <71664957+Christian-Mar@users.noreply.github.com>
2023-03-29 21:33:09 +0200

    styling 2

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -26,78 +26,91 @@ const MenuBar = ({ editor }) => {
 	}
 
 	return (
-		<>
-			<button
-				onClick={() => editor.chain().focus().toggleBold().run()}
-				disabled={!editor.can().chain().focus().toggleBold().run()}
-				className={editor.isActive('bold') ? 'is-active' : ''}
-			>
-				<FaBold />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleItalic().run()}
-				disabled={!editor.can().chain().focus().toggleItalic().run()}
-				className={editor.isActive('italic') ? 'is-active' : ''}
-			>
-				<FaItalic />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleStrike().run()}
-				disabled={!editor.can().chain().focus().toggleStrike().run()}
-				className={editor.isActive('strike') ? 'is-active' : ''}
-			>
-				<FaStrikethrough />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-				className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
-			>
-				<FaHeading />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleBulletList().run()}
-				className={editor.isActive('bulletList') ? 'is-active' : ''}
-			>
-				<FaListUl />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleOrderedList().run()}
-				className={editor.isActive('orderedList') ? 'is-active' : ''}
-			>
-				<FaListOl />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleBlockquote().run()}
-				className={editor.isActive('blockquote') ? 'is-active' : ''}
-			>
-				<FaQuoteLeft />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().undo().run()}
-				disabled={!editor.can().chain().focus().undo().run()}
-			>
-				<FaUndo />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().redo().run()}
-				disabled={!editor.can().chain().focus().redo().run()}
-			>
-				<FaRedo />
-			</button>
-		</>
+		<div className='menu-bar'>
+			<div>
+				<button
+					onClick={() => editor.chain().focus().toggleBold().run()}
+					disabled={!editor.can().chain().focus().toggleBold().run()}
+					className={editor.isActive('bold') ? 'is-active' : ''}
+				>
+					<FaBold />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleItalic().run()}
+					disabled={!editor.can().chain().focus().toggleItalic().run()}
+					className={editor.isActive('italic') ? 'is-active' : ''}
+				>
+					<FaItalic />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleUnderline().run()}
+					disabled={!editor.can().chain().focus().toggleUnderline().run()}
+					className={editor.isActive('underline') ? 'is-active' : ''}
+				>
+					<FaUnderline />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleStrike().run()}
+					disabled={!editor.can().chain().focus().toggleStrike().run()}
+					className={editor.isActive('strike') ? 'is-active' : ''}
+				>
+					<FaStrikethrough />
+				</button>
+				<button
+					onClick={() =>
+						editor.chain().focus().toggleHeading({ level: 1 }).run()
+					}
+					className={
+						editor.isActive('heading', { level: 1 }) ? 'is-active' : ''
+					}
+				>
+					<FaHeading />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleBulletList().run()}
+					className={editor.isActive('bulletList') ? 'is-active' : ''}
+				>
+					<FaListUl />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleOrderedList().run()}
+					className={editor.isActive('orderedList') ? 'is-active' : ''}
+				>
+					<FaListOl />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleBlockquote().run()}
+					className={editor.isActive('blockquote') ? 'is-active' : ''}
+				>
+					<FaQuoteLeft />
+				</button>
+			</div>
+			<div>
+				<button
+					onClick={() => editor.chain().focus().undo().run()}
+					disabled={!editor.can().chain().focus().undo().run()}
+				>
+					<FaUndo />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().redo().run()}
+					disabled={!editor.can().chain().focus().redo().run()}
+				>
+					<FaRedo />
+				</button>
+			</div>
+		</div>
 	);
 };
 
 const Tiptap = () => {
 	const editor = useEditor({
-		extensions: [
-			StarterKit, Underline
-		],
+		extensions: [StarterKit, Underline],
 		content: ``,
 	});
 
 	return (
-		<div>
+		<div className='text__editor'>
 			<MenuBar editor={editor} />
 			<EditorContent editor={editor} />
 		</div>
